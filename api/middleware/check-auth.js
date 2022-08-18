3f8fba231c9848b30974e6265f0e0038e99066d7
yahyawiaf <abdelftt@gmail.com>
2022-08-18 16:22:17 +0200

    initial commit

---
+++ api/middleware/check-auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
+       var decoded = jwt.verify(token, process.env.JWT_KEY);
+       req.userData = decoded;
+       next();
+    } catch {
+     return res.status(200).json({
+         message: "Auth failed"
+     })
+    }
+
+}
